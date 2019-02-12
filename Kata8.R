c51e7eb521fa1c3a27e0783a4dd2414ebc2bf384
sjtouzel <37873145+sjtouzel@users.noreply.github.com>
2019-02-12 12:08:21 -0600

    started to do some R refresher stuff for fun

---
+++ Kata8.R
@@ -0,0 +1,11 @@
+# Title     : TODO
+# Objective : TODO
+# Created by: z02sjt
+# Created on: 2/12/2019
+
+# Let's do some R stuff for fun
+
+###Keep Hydrated! - we're gonna build a function that returns 1/2 of an input value and then rounds that number down
+litres <- function(time) {floor(0.5 *time) }
+
+
