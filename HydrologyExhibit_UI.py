ee642588c591d7bf2baf23b5378b63bc38b1be0e
JoeT <blandingst@gmail.com>
2019-09-24 22:57:27 -0500

    start of new UI

---
+++ HydrologyExhibit_UI.py
@@ -0,0 +1,10 @@
+# User interface for running hydro analysis on DEM data
+# and generating conservation easements for exhibits
+
+# Import Property Boundary
+
+# Set stream delineation value (examine raster calc
+# formula to figure out what this number means)
+
+# Import DEMs and mosaic any that intersect property
+
