d563e84db4f9c654d5e02a3fea2b24e1efafc4ba
JoeT <37873145+sjtouzel@users.noreply.github.com>
2019-10-29 22:14:18 -0500

    uhok whatever

--- HydrologyExhibit_UI.py
+++ HydrologyExhibit_UI.py
@@ -2,9 +2,11 @@
 # and generating conservation easements for exhibits
 
 # Import Property Boundary
+propBoundary = r"D:/SUNOCO_ETP/HELP_DOCS/COMPLIANCE/QuadMaps/SHP/"
 
 # Set stream delineation value (examine raster calc
 # formula to figure out what this number means)
+streamDelinThreshold = 3.5
 
 # Import DEMs and mosaic any that intersect property
 
