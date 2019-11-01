64bd14bdb3fa8dce86d96a8dbcf192482095555f
JoeT <37873145+sjtouzel@users.noreply.github.com>
2019-11-01 15:18:40 -0500

    creating a bunch of gdbs and fcs with domain'd field

--- CreateFoldersWithGDBs.py
+++ CreateFoldersWithGDBs.py
@@ -26,6 +26,15 @@ for i in DomainBanksList:
     arcpy.CreateFileGDB_management(path, i + "Collector_Field.gdb")
     print(i + "Collector_Field.gdb")
 
+# For each geodatabase create the Photo_Direction domain
+domName = "PhotoDirection"
+for i in GDBPathsList:
+    arcpy.CreateDomain_management(i, domName, "Cardinal Direction of the Photo", "TEXT", "CODED")
+    domDict = {"N":"N", "E":"E", "S":"S", "W":"W"}
+    for code in domDict:
+        arcpy.AddCodedValueToDomain_management(i,domName, code, domDict[code])
+    print(i)
+
 # For each Geodatabase create a line, point, and polygon layer
 LineTemplate = r"E:\Dropbox (RES)\@RES GIS\projects\CA\Domain\GDB\PajaroCollector_Field.gdb\Line"
 PolygonTemplate = r"E:\Dropbox (RES)\@RES GIS\projects\CA\Domain\GDB\PajaroCollector_Field.gdb\Polygon"
@@ -34,7 +43,11 @@ has_m = "DISABLED"
 has_z = "DISABLED"
 
 for i in GDBPathsList:
-    arcpy.CreateFeatureclass_management(GDBPathsList[0],"Point", "POINT", PointTemplate, has_m, has_z, prj10)
+    arcpy.CreateFeatureclass_management(i,"Point", "POINT", PointTemplate, has_m, has_z, prj10)
+    pointPath = os.path.join(i,"Point")
+    arcpy.AssignDomainToField_management(pointPath, "PHOTO_DIRECTION", domName)
+    arcpy.CreateFeatureclass_management(i,"Line", "POLYLINE", LineTemplate, has_m, has_z, prj10)
+    arcpy.CreateFeatureclass_management(i, "Polygon", "POLYGON", PolygonTemplate, has_m, has_z, prj10)
     print(i)
 
 
