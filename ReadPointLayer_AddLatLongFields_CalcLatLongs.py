64e34c1598eb06099f9b8a1a4f36e11ceb345686
JoeT <37873145+sjtouzel@users.noreply.github.com>
2019-11-19 11:17:08 -0600

    more development

---
+++ ReadPointLayer_AddLatLongFields_CalcLatLongs.py
@@ -0,0 +1,34 @@
+#### We're going to check all these geodatabases, get the point layer, add a lat and a long field, then calc the lat and longs
+import os
+from arcpy import env
+import arcpy
+import datetime
+import time
+
+###### Get date for tagging our output files ######
+dateTag = datetime.datetime.today().strftime('%Y%m%d') # looks somethin like this 20181213
+
+###### Get the folder path for all our stuff ######
+MotherShipFolder = r"C:/Users/jtouzel/Downloads/DomainFieldDataDownload"
+calcCS = r"C:/Users/jtouzel/AppData/Roaming/Esri/Desktop10.6/ArcMap/Coordinate Systems/WGS 1984.prj"
+folderlist = [] # Create empty list for all the sub-folder paths
+for i in os.listdir(MotherShipFolder): # get a list of all the sub-folders
+    folderlist.append(MotherShipFolder + "/" + i)
+for i in folderlist: # get into each sub-folder
+    for n in os.listdir(i):# get the geodatabase out of the sub-folder
+        if n.endswith('.gdb'):
+            env.workspace = os.path.join(i,n) # set the gdb as the workspace
+            for fc in arcpy.ListFeatureClasses(): # find the point layer
+                desc = arcpy.Describe(fc) # first let's check if there's lat and long fields
+                flds = desc.fields
+                for fld in flds: # if they are there we need to delete them
+                    if fld == "LAT":
+                        arcpy.Delete_management(fc, ["LAT", "LONG"])
+                #path = os.path.join(env.workspace, fc)
+                arcpy.AddField_management(fc, "LAT", "DOUBLE")
+                arcpy.AddField_management(fc, "LONG", "DOUBLE")
+                arcpy.env.outputCoordinateSystem = calcCS
+                arcpy.CalculateGeometryAttributes_management(fc, [["LAT", "POINT_Y"],["LONG", "POINT_X"]])
+
+FC_List = arcpy.ListFeatureClasses()
+arcpy.ListDatasets()
