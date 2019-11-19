50fe1c082518204903a816177784dbc1c51e9344
JoeT <37873145+sjtouzel@users.noreply.github.com>
2019-11-19 15:03:15 -0600

    more development

--- DownloadCollectorPhotos_GetAttributeInfoFromTable_AppendToOutputPhotoFileName.py
+++ DownloadCollectorPhotos_GetAttributeInfoFromTable_AppendToOutputPhotoFileName.py
@@ -3,7 +3,7 @@ from arcpy import da
 from arcpy import env
 import os
 
-MotherShipFolder = r"C:/Users/jtouzel/Downloads/DomainFieldDataDownload"
+MotherShipFolder = r"C:/Users/jtouzel/Downloads/DomainFieldDataDownload" # All our downloaded data is here
 folderlist = [] # Create empty list for all the sub-folder paths
 for i in os.listdir(MotherShipFolder): # get a list of all the sub-folders
     folderlist.append(MotherShipFolder + "/" + i)
@@ -11,18 +11,20 @@ for i in folderlist: # get into each sub-folder
     for n in os.listdir(i):# get the geodatabase out of the sub-folder
         if n.endswith('.gdb'):
             env.workspace = os.path.join(i,n) # set the gdb as the workspace
-            for table in arcpy.ListTables():
-                with da.SearchCursor(table, ['DATA', 'ATT_NAME', 'REL_GLOBALID']) as cursor:
+            for table in arcpy.ListTables(): # get the attachments table
+                with da.SearchCursor(table, ['DATA', 'ATT_NAME', 'REL_GLOBALID']) as cursor: # get this data from each row
                     count = 0
                     for item in cursor:
-                        attachment = item[0]
-                        fcList = arcpy.ListFeatureClasses()
-                        with arcpy.da.SearchCursor(fcList[0], ["COMMENT", "PHOTO_DIRECTION"], "{0} = '{1}'".format("GlobalID", item[2])) as cursor2:
+                        attachment = item[0] # get the blob
+                        fcList = arcpy.ListFeatureClasses() # get a list of feature classes
+                        with arcpy.da.SearchCursor(fcList[0], ["COMMENT", "PHOTO_DIRECTION"], "{0} = '{1}'".format("GlobalID", item[2])) as cursor2: # look through point FC to get
+                                                # the related info for each photo
                             for row2 in cursor2:
                                 PhotoComment = str(row2[0])
-                                photoname = PhotoComment.replace(" ", "_") + "_" + str(row2[1]) + "_"
-                        filename = photoname + str(item[1])
-                        print(filename)
+                                photoname = PhotoComment.replace(" ", "_") + "_" + str(row2[1]) # create a photoname based on point attributes
+                        AttName = str(item[1])
+                        filename = "Count" + str(count) + "_" + photoname + ".jpg" # create the complete file name for each photo
+                        print(filename) # see what the name looks like
                         open(os.path.join(i, filename), 'wb').write(attachment.tobytes())
                         del item
                         del filename
