48dc178c7afb18fa6807792915aa8917a64aff1d
JoeT <37873145+sjtouzel@users.noreply.github.com>
2019-12-20 17:01:16 -0600

    new shit started

---
+++ RES_Scripts/FindContiguousParcelsWithMostStreamFootage.py
@@ -0,0 +1,14 @@
+# We need to find zones within a set of parcels that contain a certain amount of stream length
+import arcpy
+
+
+# Import the parcels and the stream footage
+
+
+# Two options:
+
+## Option 1: find a parcel and then select equal areas around it to find the most stream LF until you find the LF you're looking for
+
+
+## Option 2: use an increasing in size fishnet to find areas that fit the LF you're looking for
+
