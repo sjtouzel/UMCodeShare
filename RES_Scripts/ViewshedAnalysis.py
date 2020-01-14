98189313ee35daf136ff1eb3c7f34b2c0e77773f
JoeT <37873145+sjtouzel@users.noreply.github.com>
2020-01-14 09:22:58 -0600

    new project

--- RES_Scripts/ViewshedAnalysis.py
+++ RES_Scripts/ViewshedAnalysis.py
@@ -23,33 +23,33 @@ Inputs:
 - output folder
 """
 # Scratch workspace
-arcpy.env.scratchWorkspace = r"C:"
-scratchGDB = arcpy.env.scratchGDB # Path to scratch GDB
-outPathScratch = scratchGDB
+arcpy.env.scratchWorkspace = r"C:"   # *****
+scratchGDB = arcpy.env.scratchGDB # Path to scratch GDB  # *****
+outPathScratch = scratchGDB  # *****
 
 # Standard projection
-ProjectAll = r"C:\Users\jtouzel\AppData\Roaming\Esri\Desktop10.6\ArcMap\Coordinate Systems\NAD 1983 UTM Zone 10N.prj"
+ProjectAll = r"C:\Users\jtouzel\AppData\Roaming\Esri\Desktop10.6\ArcMap\Coordinate Systems\NAD 1983 UTM Zone 10N.prj"  # *****
 
 # Get DEM to use
-DEM_GDB = r"E:\Dropbox (RES)\@RES GIS\projects\CA\Klamath\DEM\USGS_NED\USGS_NED.gdb"
+DEM_GDB = r"E:\Dropbox (RES)\@RES GIS\projects\CA\Klamath\DEM\USGS_NED\USGS_NED.gdb"  # *****
 env.workspace = DEM_GDB
 FC_List_DEM = arcpy.ListDatasets()
 DEM = os.path.join(DEM_GDB, FC_List_DEM[3])
 descDEM = arcpy.Describe(DEM)
 descDEM.spatialReference.name
 # project DEM to UTM zone 10N
-DEM_Proj = os.path.join(outPathScratch, "Klamath_DEM_Proj_v2")
+DEM_Proj = os.path.join(outPathScratch, "Klamath_DEM_Proj_v2")  # *****
 arcpy.ProjectRaster_management(DEM, DEM_Proj, ProjectAll)
 
 
 # Get the point layer and create a cursor to go through the points to run the analysis
-PointLayerGDB = r"E:\Dropbox (RES)\@RES GIS\projects\CA\Klamath\SHP\EagleTerritoryDesignation_20191227\EagleTerritoryData.gdb"
+PointLayerGDB = r"E:\Dropbox (RES)\@RES GIS\projects\CA\Klamath\SHP\EagleTerritoryDesignation_20191227\EagleTerritoryData.gdb"  # *****
 env.workspace = PointLayerGDB
 FC_List = arcpy.ListFeatureClasses()
-PointLayer = os.path.join(PointLayerGDB, FC_List[0])
-descFC = arcpy.Describe(PointLayer)
+PointLayer = os.path.join(PointLayerGDB, FC_List[0])  # *****
+descFC = arcpy.Describe(PointLayer) # *****
 descFC.spatialReference.name
-OID_field = descFC.OIDFieldName
+OID_field = descFC.OIDFieldName  # *****
 [f.name for f in arcpy.ListFields(PointLayer)] #check what fields are in this layer
 
 PointNameList = []
@@ -74,14 +74,21 @@ with da.SearchCursor(PointLayer, [OID_field, 'RES_NAME', 'RES_NOTE']) as cursor:
             #     break
 
             #arcpy.CopyFeatures_management(PointLayer, 'Output_' + str(row[0]))  # output feature to new feature class
+arcpy.env.overwriteOutput = True
 for n in PointNameList:
     filename = n.replace(" ", "_").replace(".", "_").replace(",","_")
+    print(filename)
     outfc = os.path.join(scratchGDB, filename)
     where_clause = '"RES_NAME" = \'%s\'' % n
     arcpy.Select_analysis(PointLayer, outfc, where_clause)
     viewshed_RasterFC = 'ViewshedRaster_' + filename
     print("Running ViewShed Analysis")
     arcpy.Viewshed_3d(DEM_Proj, PointLayer, viewshed_RasterFC)
+    print("Running Raster to Polygon")
+    viewshed_PolyFC = 'ViewshedPoly_' + filename
+    arcpy.RasterToPolygon_conversion(viewshed_RasterFC, viewshed_PolyFC, "SIMPLIFY", "gridcode", "MULTIPLE_OUTER_PART")
+    if count > 1:
+        break
 
 
 
