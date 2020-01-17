3416cb4af69669573848c67cd3bb63af9d6a4858
JoeT <37873145+sjtouzel@users.noreply.github.com>
2020-01-17 15:36:18 -0600

    new project

--- CodeTesting.py
+++ CodeTesting.py
@@ -1,4 +1,6 @@
 import arcpy
+from arcpy import env
+import time, os
 
 featureClass = ['in', 'out', 'up', 'down']
 print("_".join(featureClass))
@@ -12,3 +14,18 @@ newlist = [item1, item2, item3]
 newlist2 = [i for i in newlist if i]
 if item2:
     print(True)
+
+
+# extract raster by mask
+arcpy.env.scratchWorkspace = r"C:"
+scratchGDB = arcpy.env.scratchGDB # Path to scratch GDB
+arcpy.env.overwriteOutput = True
+nlcdGDB = r"E:\Dropbox (RES)\@RES GIS\Data\OK\OK_Vector\NLCD\OK_NLCD.gdb"
+env.workspace = nlcdGDB
+fc_list = arcpy.ListDatasets()
+nlcd = os.path.join(nlcdGDB, fc_list[0])
+maskGDB = r"E:\Dropbox (RES)\@RES GIS\projects\OK\PRJ101582_AEP_ABB_HCP_Phase1\SHP\ProjectData_20200116.gdb"
+env.workspace = maskGDB
+fc_list2 = arcpy.ListFeatureClasses()
+mask = os.path.join(maskGDB, fc_list2[0])
+outExtractByMask = arcpy.sa.ExtractByMask(nlcd, mask)
