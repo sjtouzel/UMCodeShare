f392215ec4fa6ce1300febcb512186fa6165e891
JoeT <37873145+sjtouzel@users.noreply.github.com>
2020-07-01 23:13:48 -0500

    multi input testing

--- RES_Scripts/MultiInputTesting.py
+++ RES_Scripts/MultiInputTesting.py
@@ -2,4 +2,6 @@ import arcpy
 
 MultiInputs = arcpy.GetParameterAsText(0) # Get all the layers
 
-arcpy.AddMessage(MultiInputs)
+MultiInputsList = MultiInputs.split(";")  # multi value input needs to be parsed
+for i in MultiInputsList:
+    arcpy.AddMessage(i)
