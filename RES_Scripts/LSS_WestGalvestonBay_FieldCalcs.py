e85ae7d6df2d6519bc0066d353f0186e0b1b7de1
JoeT <37873145+sjtouzel@users.noreply.github.com>
2020-07-30 22:52:50 -0500

    creating Galveston Bay ranking script

---
+++ RES_Scripts/LSS_WestGalvestonBay_FieldCalcs.py
@@ -0,0 +1,37 @@
+# Return a score that combines all our ranking fields
+def rank(streams, canopyCoverP, canopyCoverB, landCoverP, landCoverB):
+    # Set percentage multipliers
+    stream_per = 15
+    other_per = 2.5
+
+    if streams is 0:
+        return 0
+    else:
+        perCalc = streams*stream_per + canopyCoverP*other_per + canopyCoverB*other_per + \
+                  landCoverP*other_per + landCoverB*other_per
+        return perCalc
+
+rank(!Stream_Linear_FeetR!,!Canopy_cover_parcelR!,!Canopy_cover_riparian_bufferR!,!LULC_parcelR!,!LULC_bufferR!)
+
+# convert null values to 0 for those fields that we're using to calc ranks
+def calc(field):
+    if field is None:
+        return 0
+    else:
+        return field
+
+# calculate the priority field letter scores
+def priority(rankField):
+    if rankField == 0:
+        return None
+    elif rankField >= 90:
+        return 'A'
+    elif rankField > 80:
+        return 'B'
+    elif rankField >= 70:
+        return 'C'
+    elif rankField >= 40:
+        return 'D'
+    else:
+        return 'E'
+
