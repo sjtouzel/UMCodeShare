052bb1695350370c336b96df231f91adaf1e7c13
JoeT <37873145+sjtouzel@users.noreply.github.com>
2020-12-31 12:47:48 -0600

    multi input testing

--- TestingMultiValueInputs.py
+++ TestingMultiValueInputs.py
@@ -3,5 +3,7 @@ import os, time, datetime
 
 FeatureClass = arcpy.GetParameterAsText(0)
 ListOfFields = arcpy.GetParameterAsText(1)
+ListOfFieldTypes = arcpy.GetParameterAsText(2)
 
 arcpy.AddMessage(ListOfFields)
+arcpy.AddMessage(ListOfFieldTypes)
